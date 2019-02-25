452d9dfd2ff01240c605683b481bff42e3ea4895
hobbyquaker <bastiraff@googlemail.com>
2019-02-25 22:16:40 +0100

    try to fix head2head prevention 🧟‍

--- index.js
+++ index.js
@@ -188,16 +188,16 @@ app.post('/move', (request, response) => {
                 }
             }
             board[c.x][c.y] = 0;
-            if (x === c.x && y === (c.y + 1)) {
+            if (board[x] === 0 && board[y - 1] === 0) {
                 possible.delete('up');
             }
-            if (x === c.x && y === (c.y - 1)) {
+            if (board[x] === 0 && board[y + 1] === 0) {
                 possible.delete('down');
             }
-            if (y === c.y && x === (c.x + 1)) {
+            if (board[x - 1] === 0 && board[y] === 0) {
                 possible.delete('left');
             }
-            if (y === c.y && x === (c.x - 1)) {
+            if (board[x + 1] === 0 && board[y] === 0) {
                 possible.delete('right');
             }
         });
